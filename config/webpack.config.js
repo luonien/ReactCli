756e62eab369c1630f422ed9f886f32688c145b3
“luonien” <58380039+luonien@users.noreply.github.com>
2023-03-24 12:02:28 +0800

    fix:将react相关的库单独打包，减少node_modules的chunk体积

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -129,6 +129,29 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
+      cacheGroups:{
+        // 如果项目中使用antd，此时将所有node_modules打包在一起，那么打包输出文件会比较大。
+        // 所以我们将node_modules中比较大的模块单独打包，从而并行加载速度更好
+        // 如果项目中没有，请删除
+        // antd: {
+        //     name: "chunk-antd",
+        //     test: /[\\/]node_modules[\\/]antd(.*)/,
+        //     priority: 30,
+        //   },
+          // 将react相关的库单独打包，减少node_modules的chunk体积。
+          react: {
+            name: "react",
+            test: /[\\/]node_modules[\\/]react(.*)?[\\/]/,
+            chunks: "initial",
+            priority: 20,
+          },
+          libs: {
+            name: "chunk-libs",
+            test: /[\\/]node_modules[\\/]/,
+            priority: 10, // 权重最低，优先考虑前面内容
+            chunks: "initial",
+          },
+      }
     },
     runtimeChunk: {
       name: (entrypoint) => `runtime~${entrypoint.name}.js`,
@@ -175,5 +198,6 @@ module.exports = {
         open:true,
         hot:true, //开启热模块替换
         historyApiFallback:true, //解决前端路由刷新显示404问题
-      }
+      },
+      performance:false, //关闭性能分析，提升打包速度
 }
